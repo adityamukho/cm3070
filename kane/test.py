f92765d35044b60a795c622beb8c51b04c0412bc
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-17 16:19:58 +0530

    Sleep for 1 sec before next reading.

--- kane/test.py
+++ kane/test.py
@@ -1,3 +1,5 @@
+import time
+
 from tmrl.custom.utils.tools import TM2020OpenPlanetClient
 
 
@@ -6,3 +8,4 @@ client = TM2020OpenPlanetClient()
 while True:
     data = client.retrieve_data(sleep_if_empty=0.01)
     print(data)
+    time.sleep(1.0)
