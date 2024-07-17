c25107e863f3ac9022d6ad7cb1fd08a627cf442d
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-17 11:48:59 +0530

    Testing connection and data dump.

---
+++ kane/test.py
@@ -0,0 +1,8 @@
+from tmrl.custom.utils.tools import TM2020OpenPlanetClient
+
+
+client = TM2020OpenPlanetClient()
+
+while True:
+    data = client.retrieve_data(sleep_if_empty=0.01)
+    print(data)
