d584604336f8e2a39e5aaece642bb718dba9d266
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-18 08:14:17 +0530

    Fixed failing assertions.

--- test/test_data.py
+++ test/test_data.py
@@ -5,10 +5,10 @@ from tmrl.custom.utils.tools import TM2020OpenPlanetClient
 client = TM2020OpenPlanetClient()
 data = client.retrieve_data(sleep_if_empty=0.01)
 
-assert isinstance(data, list)
+assert isinstance(data, tuple)
 assert len(data) == 11
 assert all(isinstance(d, float) for d in data)
-assert data[0] == 0.0 # speed is 0
+assert data[0] < 0.01 # speed is ~0
 assert data[1] == 0.0 # total distance travelled is 0
 # data[2:5] == x, y, z coordinates
 assert data[5] == 0.0 # left/right not activated
