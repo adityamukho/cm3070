4a0738b567fe3f2f45c552d79824f1c6d758bbbd
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-18 07:28:28 +0530

    Try sending a control.

---
+++ README.md
@@ -0,0 +1,13 @@
+# TMRL Data
+
+0. speed
+1. total distance travelled
+2. x
+3. y
+4. z
+5. left/right
+6. fwd/back
+7. is_braking
+8. is_finished
+9. gear
+10. rpm
--- kane/test.py
+++ kane/test.py
@@ -1,11 +1,13 @@
 import time
 
 from tmrl.custom.utils.tools import TM2020OpenPlanetClient
-
+from vgamepad import VX360Gamepad
 
 client = TM2020OpenPlanetClient()
+gamepad = VX360Gamepad()
 
 while True:
     data = client.retrieve_data(sleep_if_empty=0.01)
-    print(data)
-    time.sleep(1.0)
+    gamepad.right_trigger_float(value_float=1.0)
+    time.sleep(0.01)
+
