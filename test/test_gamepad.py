25bdb918900e04b2f00c1f8f286b7ad276302327
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-18 08:49:45 +0530

    Try to get the gamepad to self-destruct before interpreter shutdown begins.

--- test/test_gamepad.py
+++ test/test_gamepad.py
@@ -1,23 +1,29 @@
-import time
-
 from tmrl.custom.utils.tools import TM2020OpenPlanetClient
-from vgamepad import VX360Gamepad
 
 client = TM2020OpenPlanetClient()
-gamepad = VX360Gamepad()
+
+
+def use_gamepad():
+    import time
+    from vgamepad import VX360Gamepad
+
+    gamepad = VX360Gamepad()
+    end_time = time.time() + 1
+    data = []
+
+    while time.time() < end_time:
+        gamepad.right_trigger_float(value_float=1.0)  # gas
+        gamepad.left_trigger_float(value_float=0.0)  # brake
+        gamepad.left_joystick_float(0.0, 0.0)  # left/right
+        gamepad.update()
+        data = client.retrieve_data(sleep_if_empty=0.01)
+        time.sleep(0.01)
+
+    return data
+
+
 data0 = client.retrieve_data(sleep_if_empty=0.01)
-end_time = time.time() + 1
-data1 = []
-
-while time.time() < end_time:
-    gamepad.right_trigger_float(value_float=1.0)  # gas
-    gamepad.left_trigger_float(value_float=0.0)  # brake
-    gamepad.left_joystick_float(0.0, 0.0)  # left/right
-    gamepad.update()
-    data1 = client.retrieve_data(sleep_if_empty=0.01)
-    time.sleep(0.01)
-
-gamepad.reset()
+data1 = use_gamepad()
 
 print(data0)
 print(data1)
@@ -30,5 +36,3 @@ assert data0[4] != data1[4]  # z coordinate has changed
 assert data1[5] == 0  # left/right not activated
 assert data1[6] == 1.0  # forward full thrust
 assert data1[7] == 0.0  # brakes not engaged
-
-time.sleep(0.5)
