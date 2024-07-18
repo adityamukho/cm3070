a5b574563dfd36fdeff374d4ae829dfb44909ee4
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-18 08:56:32 +0530

    More robust coordinate check.

--- test/test_gamepad.py
+++ test/test_gamepad.py
@@ -30,9 +30,10 @@ print(data1)
 
 assert data1[0] > 0  # speed > 0
 assert data1[1] > 0  # distance covered > 0
-assert data0[2] != data1[2]  # x coordinate has changed
-# y coordinate may or may not have changed
-# z coordinate may or may not have changed
+
+# At least one of x, y or z coordinates has changed
+assert data0[2] != data1[2] or data0[3] != data1[3] or data0[4] != data1[4]
+
 assert data1[5] == 0  # left/right not activated
 assert data1[6] == 1.0  # forward full thrust
 assert data1[7] == 0.0  # brakes not engaged
