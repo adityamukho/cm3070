f1f6ace15df4947ed51a1b495a06a1540f1afdb3
Aditya Mukhopadhyay <webmaster@adityamukho.com>
2024-07-18 07:30:27 +0530

    Stop on game end.

--- kane/test.py
+++ kane/test.py
@@ -5,9 +5,12 @@ from vgamepad import VX360Gamepad
 
 client = TM2020OpenPlanetClient()
 gamepad = VX360Gamepad()
+data = []
 
-while True:
+while not len(data) or data[8] == 0.0:
     data = client.retrieve_data(sleep_if_empty=0.01)
     gamepad.right_trigger_float(value_float=1.0)
     time.sleep(0.01)
+
+print('Finished.')
 
